603cb3c7165196b66b60e3fe1e7b8b3931de4bd0
Apex <apexpredator@Apexs-MacBook-Pro.local>
2022-01-20 14:19:53 +0530

    before launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:assistive_app/Login/login.dart';
-import 'package:assistive_app/Screen/Screen1.dart';
 import 'package:assistive_app/Subject/subject.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+int? loggedIn;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences localStorage = await SharedPreferences.getInstance();
+
+  loggedIn = await localStorage.getInt('userId');
+
   runApp(MyApp());
 }
 
@@ -13,56 +23,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'assistive APP',
-      theme: ThemeData(),
-      home: CheckAuth(),
-    );
-  }
-}
-
-class CheckAuth extends StatefulWidget {
-  @override
-  _CheckAuthState createState() => _CheckAuthState();
-}
-
-class _CheckAuthState extends State<CheckAuth> {
-  bool isAuth = false;
-  @override
-  void initState() {
-    _checkIfLoggedIn();
-    super.initState();
-  }
-
-  void _checkIfLoggedIn() async {
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    var token = localStorage.getString('userName');
-    var userId = localStorage.getInt('userId');
-
-    if (token != null) {
-      setState(() {
-        isAuth = true;
-        print(localStorage.getString('userName'));
-        print(localStorage.getInt('userId'));
-        print(userId);
-        print("Successfuly loggedin");
-      });
-    } else {
-      print(localStorage.getString('token'));
-      print("Successfuly loggedin");
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Widget child;
-    if (isAuth) {
-      child = SubjectPage();
-    } else {
-      child = LoginPage();
-    }
-    return Scaffold(
-      body: child,
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Assistive APP',
+        theme: ThemeData(),
+        initialRoute:
+            loggedIn == 0 || loggedIn == null ? 'Login' : 'SubjectPage',
+        routes: {
+          'Login': (context) => LoginPage(),
+          'SubjectPage': (context) => SubjectPage(),
+        });
   }
 }
